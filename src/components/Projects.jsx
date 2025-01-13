bc9fa81221bb1389c8c29a76defb3e0a6b64e2a4
Vipin Yadav <yadavipin57@gmail.com>
2025-01-13 15:31:39 +0530

    projects made dynamic

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,3 +1,10 @@
+import {
+  projectNames,
+  projectLink,
+  projectDescription,
+  projectPagesSRC,
+} from "../utils/constants";
+
 const Projects = () => {
   return (
     <div className="bg-[#303030]" id="projects">
@@ -7,217 +14,38 @@ const Projects = () => {
             Projects
           </h2>
         </div>
-        <div className=" px-4 sm:px-8 md:px-16 py-8 text-white flex flex-col sm:flex-row gap-6 sm:gap-4 md:gap-8 items-center justify-between flex-wrap">
-          {/* PROJECT 1 */}
-          <div className="w-[80%] sm:w-[46%] h-[260px] text-black border-[2px] rounded-lg border-[#E1451D] cursor-pointer relative group">
-            {/* Hoverable Content */}
-            <div className="absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]">
-              {/* Visible on hover */}
-              <h4 className="mx-auto w-full text-center text-white text-2xl font-bold group-hover:block" title="Visit site">
-                <a
-                  className="hover:underline"
-                  href="https://balloonburstgame.netlify.app"
-                  target="_blank"
-                >
-                  Balloon Burst Game
-                </a>
-              </h4>
-              {/* Initially hidden, visible on hover */}
-              <p className="text-white hidden md:group-hover:block  text-justify">
-                A Balloon Burst 2D game created using Canva and Javascript.
-              </p>
-            </div>
-          </div>
+        <div className="py-8 text-white flex flex-col sm:flex-row gap-6 sm:gap-4 md:gap-8 items-center justify-center flex-wrap relative">
 
-          {/* PROJECT 2 */}
-          <div className="w-[80%] sm:w-[46%] h-[260px] text-black border-[2px] rounded-lg border-[#E1451D] cursor-pointer relative group">
-            {/* Hoverable Content */}
-            <div className="absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]">
-              {/* Visible on hover */}
-              <h4 className="mx-auto w-full text-center text-white text-2xl font-bold group-hover:block" title="Visit site">
-                <a
-                  className="hover:underline"
-                  href="https://netflixgpt-747.web.app/"
-                  target="_blank"
-                >
-                  NetflixGPT
-                </a>
-              </h4>
-              {/* Initially hidden, visible on hover */}
-              <p className="text-white hidden md:group-hover:block  text-justify">
-                A movie recommendation web app using the OpenAI and TMDB API.
-              </p>
-            </div>
-          </div>
-
-          {/* PROJECT 3 */}
-          <div className="w-[80%] sm:w-[46%] h-[260px] text-black border-[2px] rounded-lg border-[#E1451D] cursor-pointer relative group">
-            {/* Hoverable Content */}
-            <div className="absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]">
-              {/* Visible on hover */}
-              <h4 className="mx-auto w-full text-center text-white text-2xl font-bold group-hover:block" title="Visit site">
-                <a
-                  className="hover:underline"
-                  href="https://reactchat147.web.app/"
-                  target="_blank"
-                >
-                  React Chat App
-                </a>
-              </h4>
-              {/* Initially hidden, visible on hover */}
-              <p className="text-white hidden md:group-hover:block  text-justify">
-                A chat web application created using React.js, Firebase Authentication, Firestore and Database. 
-              </p>
-            </div>
-          </div>
-
-          {/* PROJECT 4 */}
-          <div className="w-[80%] sm:w-[46%] h-[260px] text-black border-[2px] rounded-lg border-[#E1451D] cursor-pointer relative group">
-            {/* Hoverable Content */}
-            <div className="absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]">
-              {/* Visible on hover */}
-              <h4 className="mx-auto w-full text-center text-white text-2xl font-bold group-hover:block" title="Visit site">
-                <a
-                  className="hover:underline"
-                  href="#"
-                  target="_blank"
-                >
-                  Employee Management System
-                </a>
-              </h4>
-              {/* Initially hidden, visible on hover */}
-              <p className="text-white hidden md:group-hover:block  text-justify">
-                A Employee Management System created using React.js, MySQL, Express and Node.js.
-              </p>
-            </div>
-          </div>
-
-          {/* PROJECT 5 */}
-          <div className="w-[80%] sm:w-[46%] h-[260px] text-black border-[2px] rounded-lg border-[#E1451D] cursor-pointer relative group">
-            {/* Hoverable Content */}
-            <div className="absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]">
-              {/* Visible on hover */}
-              <h4 className="mx-auto w-full text-center text-white text-2xl font-bold group-hover:block" title="Visit site">
-                <a
-                  className="hover:underline"
-                  href="https://yadavipin57.github.io/search-github-profile/"
-                  target="_blank"
-                >
-                  GitHub Profile Info
-                </a>
-              </h4>
-              {/* Initially hidden, visible on hover */}
-              <p className="text-white hidden md:group-hover:block  text-justify">
-                Live GitHub profile information of users using username.
-              </p>
-            </div>
-          </div>
-
-          {/* PROJECT 6 */}
-          <div className="w-[80%] sm:w-[46%] h-[260px] text-black border-[2px] rounded-lg border-[#E1451D] cursor-pointer relative group">
-            {/* Hoverable Content */}
-            <div className="absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]">
-              {/* Visible on hover */}
-              <h4 className="mx-auto w-full text-center text-white text-2xl font-bold group-hover:block" title="Visit site">
-                <a
-                  className="hover:underline"
-                  href="https://yadavipin57.github.io/Weather/ "
-                  target="_blank"
-                >
-                  Weather Web App
-                </a>
-              </h4>
-              {/* Initially hidden, visible on hover */}
-              <p className="text-white hidden md:group-hover:block  text-justify">
-                Live weather information of various cities around the world with a simple search.
-              </p>
-            </div>
-          </div>
-
-          {/* PROJECT 7 */}
-          <div className="w-[80%] sm:w-[46%] h-[260px] text-black border-[2px] rounded-lg border-[#E1451D] cursor-pointer relative group">
-            {/* Hoverable Content */}
-            <div className="absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]">
-              {/* Visible on hover */}
-              <h4 className="mx-auto w-full text-center text-white text-2xl font-bold group-hover:block" title="Visit site">
-                <a
-                  className="hover:underline"
-                  href="https://yadavipin57.github.io/GymFitness/"
-                  target="_blank"
-                >
-                  Gym Fitness Website
-                </a>
-              </h4>
-              {/* Initially hidden, visible on hover */}
-              <p className="text-white hidden md:group-hover:block  text-justify">
-                A Gym website created using HTML5, CSS3 and Javascript with responsive interface.
-              </p>
-            </div>
-          </div>
-
-          {/* PROJECT 8 */}
-          <div className="w-[80%] sm:w-[46%] h-[260px] text-black border-[2px] rounded-lg border-[#E1451D] cursor-pointer relative group">
-            {/* Hoverable Content */}
-            <div className="absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]">
-              {/* Visible on hover */}
-              <h4 className="mx-auto w-full text-center text-white text-2xl font-bold group-hover:block" title="Visit site">
-                <a
-                  className="hover:underline"
-                  href="https://yadavipin57.github.io/To-Do-List/"
-                  target="_blank"
-                >
-                  ToDo List
-                </a>
-              </h4>
-              {/* Initially hidden, visible on hover */}
-              <p className="text-white hidden md:group-hover:block  text-justify">
-                A ToDo List application to keep track of your activities.
-              </p>
-            </div>
-          </div>
-
-          {/* PROJECT 9 */}
-          <div className="w-[80%] sm:w-[46%] h-[260px] text-black border-[2px] rounded-lg border-[#E1451D] cursor-pointer relative group">
-            {/* Hoverable Content */}
-            <div className="absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]">
-              {/* Visible on hover */}
-              <h4 className="mx-auto w-full text-center text-white text-2xl font-bold group-hover:block" title="Visit site">
-                <a
-                  className="hover:underline"
-                  href="https://yadavipin57.github.io/Quiz-Game/"
-                  target="_blank"
-                >
-                  Quizz Game
-                </a>
-              </h4>
-              {/* Initially hidden, visible on hover */}
-              <p className="text-white hidden md:group-hover:block  text-justify">
-                A Quizz game to test your knowledge of the world.
-              </p>
-            </div>
-          </div>
-
-          {/* PROJECT 10 */}
-          <div className="w-[80%] sm:w-[46%] h-[260px] text-black border-[2px] rounded-lg border-[#E1451D] cursor-pointer relative group">
-            {/* Hoverable Content */}
-            <div className="absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]">
-              {/* Visible on hover */}
-              <h4 className="mx-auto w-full text-center text-white text-2xl font-bold group-hover:block" title="Visit site">
-                <a
-                  className="hover:underline"
-                  href="https://yadavipin57.github.io/Amazon-Clone/"
-                  target="_blank"
-                >
-                  Amazon Clone
-                </a>
-              </h4>
-              {/* Initially hidden, visible on hover */}
-              <p className="text-white hidden md:group-hover:block  text-justify">
-                Amazon Clone landing page created using HTML5 and CSS3.
-              </p>
-            </div>
-          </div>
-
+          {projectNames.map((projectName, index) => {
+            return (
+              <div
+                className="mb-8 w-[280px] h-[132px] sm:w-[32vw] sm:h-[32vh] text-black rounded-lg shadow-2xl hover:shadow-3xl cursor-pointer relative group"
+                key={projectName}
+              >
+                <div className="w-full h-full absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] rounded-lg group">
+                  <img
+                    className="w-full h-full object-cover rounded-lg"
+                    src={`${projectPagesSRC[index]}.png`}
+                    alt=""
+                  />
+                  <p  className="px-1 absolute inset-0 text-center hidden group-hover:flex items-center justify-center bg-black bg-opacity-80 text-white text-lg font-semibold"
+                  >{projectDescription[index]}</p>
+                  <h4
+                    className="mt-2 mx-auto w-full text-center text-white text-lg bg-[#E1451D] rounded-lg sm:text-2xl font-bold "
+                    title="Visit site"
+                  >
+                    <a
+                      className="hover:underline"
+                      href={projectLink[index]}
+                      target="_blank"
+                    >
+                      {projectName}
+                    </a>
+                  </h4>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
@@ -225,3 +53,27 @@ const Projects = () => {
 };
 
 export default Projects;
+
+{/* <div
+  className="w-[80%] sm:w-[32vw] h-[32vh] text-black rounded-lg bg-[#E1451D] shadow-2xl hover:shadow-3xl cursor-pointer relative group"
+  key={projectName}
+>
+  <div className="absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]">
+    <img
+      className="absolute inset-0 w-full h-full object-cover z-[-1]"
+      src={`${projectPagesSRC[index]}.png`}
+      alt=""
+    />
+    <h4
+      className="mx-auto w-full text-center text-white text-2xl font-bold group-hover:block"
+      title="Visit site"
+    >
+      <a className="hover:underline" href={projectLink[index]} target="_blank">
+        {projectName}
+      </a>
+    </h4>
+    <p className="text-white hidden md:group-hover:block  text-justify">
+      {projectDescription[index]}
+    </p>
+  </div>
+</div>; */}
