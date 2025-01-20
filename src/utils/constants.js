0cdca24057429529fb7002a1d929ce458869c0c3
Vipin Yadav <yadavipin57@gmail.com>
2025-01-21 01:38:09 +0530

    emp github link added

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -11,8 +11,41 @@ export const projectNames = [
   "Amazon Clone",
 ];
 
-export const projectLink = ["https://balloonburstgame.netlify.app", "https://netflixgpt-747.web.app/", "https://reactchat147.web.app/", "#","https://yadavipin57.github.io/search-github-profile/","https://yadavipin57.github.io/Weather/ ","https://yadavipin57.github.io/GymFitness/","https://yadavipin57.github.io/To-Do-List/","https://yadavipin57.github.io/Quiz-Game/","https://yadavipin57.github.io/Amazon-Clone/"]
+export const projectLink = [
+  "https://balloonburstgame.netlify.app",
+  "https://netflixgpt-747.web.app/",
+  "https://reactchat147.web.app/",
+  "https://github.com/yadavipin57/employee-management-system-reactjs",
+  "https://yadavipin57.github.io/search-github-profile/",
+  "https://yadavipin57.github.io/Weather/ ",
+  "https://yadavipin57.github.io/GymFitness/",
+  "https://yadavipin57.github.io/To-Do-List/",
+  "https://yadavipin57.github.io/Quiz-Game/",
+  "https://yadavipin57.github.io/Amazon-Clone/",
+];
 
-export const projectDescription = ["A Balloon Burst 2D game created using Canva and Javascript.", "A movie recommendation web app using the OpenAI and TMDB API.", "A chat web application created using React.js, Firebase Authentication, Firestore and Database.", "A Employee Management System created using React.js, MySQL, Express and Node.js.", "Live GitHub profile information of users using username.", "Live weather information of various cities around the world with a simple search.", "A Gym website created using HTML5, CSS3 and Javascript with responsive interface.", "A ToDo List application to keep track of your activities.", "A Quizz game to test your knowledge of the world.", "Amazon Clone landing page created using HTML5 and CSS3."]
+export const projectDescription = [
+  "A Balloon Burst 2D game created using Canva and Javascript.",
+  "A movie recommendation web app using the OpenAI and TMDB API.",
+  "A chat web application created using React.js, Firebase Authentication, Firestore and Database.",
+  "A Employee Management System created using React.js, MySQL, Express and Node.js.",
+  "Live GitHub profile information of users using username.",
+  "Live weather information of various cities around the world with a simple search.",
+  "A Gym website created using HTML5, CSS3 and Javascript with responsive interface.",
+  "A ToDo List application to keep track of your activities.",
+  "A Quizz game to test your knowledge of the world.",
+  "Amazon Clone landing page created using HTML5 and CSS3.",
+];
 
-export const projectPagesSRC = ["balloonBurstGame", "netflixGPT", "chatApp", "ems", "githubProfileInfo", "weatherApp", "gymFitness", "todoApp", "quizApp", "amazonClone" ]
+export const projectPagesSRC = [
+  "balloonBurstGame",
+  "netflixGPT",
+  "chatApp",
+  "ems",
+  "githubProfileInfo",
+  "weatherApp",
+  "gymFitness",
+  "todoApp",
+  "quizApp",
+  "amazonClone",
+];
